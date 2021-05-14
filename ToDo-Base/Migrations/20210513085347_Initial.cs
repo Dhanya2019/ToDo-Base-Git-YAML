69198a45f805ddf78e4e3feb201d4d25c32b6117
Dhanya Gopinath <Dhanya.Gopinath@ivanti.com>
2021-05-14 21:34:43 +0530

    Initial

---
+++ ToDo-Base/Migrations/20210513085347_Initial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDo_Base.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItems",
+                columns: new[] { "Id", "IsComplete", "Name" },
+                values: new object[,]
+                {
+                    { 1L, true, "Pay Property Tax1" },
+                    { 2L, true, "Pay Property Tax2" },
+                    { 3L, false, "Pay Property Tax3" },
+                    { 4L, false, "Pay Property Tax4" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
---
+++ ToDo-Base/Models/TodoContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoDBContext : DbContext
+    {
+        public TodoDBContext(DbContextOptions<TodoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem
+                {
+                    Id = 1,
+                    Name = "Pay Property Tax1",
+                    IsComplete = true
+                }, new TodoItem
+                {
+                    Id = 2,
+                    Name = "Pay Property Tax2",
+                    IsComplete = true
+                },
+                 new TodoItem
+                 {
+                     Id = 3,
+                     Name = "Pay Property Tax3",
+                     IsComplete = false
+                 },
+                  new TodoItem
+                  {
+                      Id = 4,
+                      Name = "Pay Property Tax4",
+                      IsComplete = false
+                  }
+                );
+        }
+    }
+}
